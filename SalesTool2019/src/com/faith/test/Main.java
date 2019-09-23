19106080c6c987b5978e2784a933bd0d1f555a87
AlanMathew07 <mathewalan3@gmail.com>
2019-09-23 15:20:50 +0530

    inital commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,15 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData objSalesData = new SalesData();
+		System.out.println("Hello! Git Repository");
+		displayGreetings();
+		objSalesData.display();
+	}
+
+	private static void displayGreetings() {
+		System.out.println("Welcome");
+	}
+}
